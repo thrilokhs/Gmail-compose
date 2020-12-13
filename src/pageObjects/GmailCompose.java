750b609f59263f7603dbb7fe18354a02a962c0d9
Thrilok <t.somashekar@napoleongames.be>
2020-12-14 00:16:54 +0100

    added comments to the Pageobject methods to enable readability

--- src/pageObjects/GmailCompose.java
+++ src/pageObjects/GmailCompose.java
@@ -7,36 +7,44 @@ import helper.Generic;
 public class GmailCompose extends Base {
 	Generic gen = new Generic();
 
+	// This clicks on the Compose button on the Gmail Home page
 	public void clickCompose(String deviceName) throws Exception {
 		By linkCompose = By.xpath("//li[@class='chapternav-item chapternav-item-" + deviceName + "']");
 		gen.clickElement(linkCompose);
 	}
 
+	// This enters the email addresses in the "To" textbox
 	public void enterToEmail(String emails) throws Exception {
 		By textboxTo = By.xpath("//textarea[@name='to']");
 		gen.inputText(textboxTo, emails);
 	}
 
+	// This clicks on the Cc link beside the address textbox
 	public void clickOnCc() throws Exception {
 		By linkCc = By.xpath("//span[text()='Cc']");
 		gen.clickElement(linkCc);
 	}
 
+	// This enters the email addresses in the "Cc" textbos
 	public void enterCcEmails(String emails) throws Exception {
 		By textboxCc = By.xpath("//textarea[@name='cc']");
 		gen.inputText(textboxCc, emails);
 	}
 
+	// This clicks on the Bcc link beside the address textbox
 	public void clickOnBcc() throws Exception {
 		By linkBcc = By.xpath("//span[text()='Bcc']");
 		gen.clickElement(linkBcc);
 	}
 
+	// This enters the email addresses in the "Bcc" textbox
 	public void enterBccEmails(String emails) throws Exception {
 		By textboxBcc = By.xpath("//textarea[@name='bcc']");
 		gen.inputText(textboxBcc, emails);
 	}
 
+	// This identifies and selects the email id from the dynamically auto suggested
+	// emails
 	public void enterPartialEmail(String partialEmail, String Name) throws Exception {
 		By textboxTo = By.xpath("//textarea[@name='to']");
 		gen.inputText(textboxTo, partialEmail);
@@ -45,6 +53,7 @@ public class GmailCompose extends Base {
 		gen.clickElement(textSuggested);
 	}
 
+	// This adds the text in the Subject textbox
 	public void addSubject(String subject) throws Exception {
 		By textboxSubject = By.xpath("//input[@name='subjectbox']");
 		gen.inputText(textboxSubject, subject);
@@ -55,42 +64,52 @@ public class GmailCompose extends Base {
 		gen.inputText(textBoxEmail, emailBody);
 	}
 
+	// This adds the text in the email body
 	public void addAttachment(String path) throws Exception {
 		By linkAttach = By.xpath("//input[@name='Filedata']");
 		gen.inputText(linkAttach, path);
 	}
 
+	// This clicks the Signature link
 	public void clickOnSignature() throws Exception {
 		By linkSignature = By.xpath("//div[@class='BP aaA aMZ']");
 		gen.clickElement(linkSignature);
 	}
 
+	// This selects and inserts the signature label that is provided as an argument
 	public void addSignature(String SignatureName) throws Exception {
 		By linkAddSign = By.xpath("//div[text()='" + SignatureName + "']");
 		gen.clickElement(linkAddSign);
 	}
 
+	// This provides a boolean result if there is an error when trying to send an
+	// email without enterging email addresses
 	public boolean Noemailerror() {
 		By errmsg = By.xpath("//div[text()='Please specify at least one recipient.']");
 		boolean result = driver.findElement(errmsg).isDisplayed();
 		return result;
 	}
 
+	// This clicks the Send button
 	public void clickOnSend() throws Exception {
 		By sendLink = By.xpath("//div[text()='Send']");
 		gen.clickElement(sendLink);
 	}
 
+	// This closes the Compose email window
 	public void ClickOnCloseComposeBox() throws Exception {
 		By btnClose = By.xpath("//img[@alt='Close']");
 		gen.clickElement(btnClose);
 	}
 
+	// This closes the error pop up that appears when trying to send an email
+	// without adding an email id
 	public void ClickOkOnErrMsgBox() throws Exception {
 		By btnOk = By.xpath("//button[text()='OK']");
 		gen.clickElement(btnOk);
 	}
 
+	// This schedules Sendemail at the date and time provided in the parameters
 	public void scheduleSend(String DDBMMMBYYYY, String HHBMMBAMPM) throws Exception {
 		By dropdn = By.xpath("//div[@class='G-asx']");
 		gen.clickElement(dropdn);
@@ -133,6 +152,8 @@ public class GmailCompose extends Base {
 
 	}
 
+	// Provide the font name as the argument so that the font is selected for the
+	// email body
 	public void selectFont(String Fontname) throws Exception {
 		By textBoxEmail = By.xpath("//div[@role='textbox']");
 		gen.clickElement(textBoxEmail);
--- src/pageObjects/GmailLogin.java
+++ src/pageObjects/GmailLogin.java
@@ -7,48 +7,52 @@ import helper.Generic;
 public class GmailLogin extends Base {
 
 	Generic gen = new Generic();
-	
-	By logoutMenu = By.xpath("//img[@class='gb_Da gbii']");
-	
-	By btnSignout = By.xpath("//a[text()='Sign out']");
-	
 
-	public void enterEmail() throws Exception
-	{
+	By logoutMenu = By.xpath("//img[@class='gb_Da gbii']");
+
+	By btnSignout = By.xpath("//a[text()='Sign out']");
+
+	// This enters the email address to be logged in
+	public void enterEmail() throws Exception {
 		By emailId = By.xpath("//input[@type='email']");
 		gen.inputText(emailId, "3lokhs37@gmail.com");
 	}
-	
-	public void clickNext() throws Exception
-	{
+
+	// This clicks on the Next button after entering the email/password
+	public void clickNext() throws Exception {
 		By next = By.xpath("//div[@class='VfPpkd-RLmnJb']");
 		gen.clickElement(next);
 	}
-	
-	public void enterPassword() throws Exception
-	{
+
+	// This enters the corresponding password for the email id to be logged in
+	public void enterPassword() throws Exception {
 		By password = By.xpath("//input[@type='password']");
 		gen.inputText(password, "Passw0rd123$");
 	}
-	
-	public void clickAltLogin() throws Exception
-	{
+
+	// This clicks on the login button of the stackoverflow website
+	// Since gmail has restriction to allow login, as a workaround, a login session
+	// is created here
+	public void clickAltLogin() throws Exception {
 		By altLogin = By.xpath("//a[@class='login-link s-btn s-btn__filled py8 js-gps-track']");
 		gen.clickElement(altLogin);
 	}
-	
-	public void clickloginWithGoogle() throws Exception
-	{
+
+	// This selects and clicks the Loginwith google button of stackoverflow login
+	// page
+	public void clickloginWithGoogle() throws Exception {
 		By loginWithGoogle = By.xpath("//button[@data-provider='google']");
 		gen.clickElement(loginWithGoogle);
 	}
-	
-	
+
+	// This creates a login session through stackoverflow.com and takes to the logged
+	// in Gmail dashboard page of the user
 	public void Login() throws Exception {
-		
+
 		driver.navigate().to("https://stackoverflow.com/");
 		this.clickAltLogin();
-		this.clickloginWithGoogle();;
+		this.clickloginWithGoogle();
+		;
 		this.enterEmail();
 		this.clickNext();
 		this.enterPassword();
@@ -57,12 +61,12 @@ public class GmailLogin extends Base {
 		Thread.sleep(2000);
 		driver.get("https://mail.google.com/mail/u/0/#inbox");
 	}
-	
-	public void Logout() throws Exception
-	{   
-        gen.clickElement(logoutMenu);
-        Thread.sleep(2000);
-        gen.clickElement(btnSignout);
+
+	// This logs out the user session
+	public void Logout() throws Exception {
+		gen.clickElement(logoutMenu);
+		Thread.sleep(2000);
+		gen.clickElement(btnSignout);
 	}
 
 }
